3d8c23e61959821c657b4b93877cec43e46335eb
satoi8080 <zhangzhe.dev@outlook.com>
2022-02-18 09:52:02 +0900

    UPDATE config.py

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
 from dotenv import load_dotenv
+from distutils.util import strtobool
 
 load_dotenv()
 import_calendar_ID = os.getenv('IMPORT_CALENDAR_ID')
 export_calendar_ID = os.getenv('EXPORT_CALENDAR_ID')
 queryname = os.getenv('QUERY_NAME')
 timezone = os.getenv('TIMEZONE')
+
+clear_old_export = strtobool(os.getenv('CLEAR_OLD_EXPORT'))
+add_new_export = strtobool(os.getenv('ADD_NEW_EXPORT'))
+month_offset = int(os.getenv('MONTH_OFFSET'))
