73cc7c29c89517272b6264991cc1d7b6d77e5f3d
Zhe <zhangzhe.dev@outlook.com>
2022-05-31 14:51:11 +0900

    ADD mid-shift

--- config.py
+++ config.py
@@ -11,3 +11,7 @@ timezone = os.getenv('TIMEZONE')
 clear_old_export = strtobool(os.getenv('CLEAR_OLD_EXPORT'))
 add_new_export = strtobool(os.getenv('ADD_NEW_EXPORT'))
 month_offset = int(os.getenv('MONTH_OFFSET'))
+
+EARLY_START_TIME = '09:00'
+MID_START_TIME = '12:00'
+LATE_START_TIME = '15:00'
